f47e82e45dc8abaf435b813d83aa59dd2b0a6a7c
Mauricio Artavia Monge <martavia.dev@gmail.com>
2026-05-12 18:26:13 -0600

    style: fix indentation and remove unnecessary comment in App.java

--- src/main/java/dev/martavia/clue/App.java
+++ src/main/java/dev/martavia/clue/App.java
@@ -1,15 +1,19 @@
 package dev.martavia.clue;
+
 /**
+ * 
  * Clase App.
+ * 
  * @author Mauricio Artavia Monge
  */
 public class App {
+
     /**
      * Metodo main del programa Asistente Virtual CLUE.
+     * 
      * @param args Argumentos del programa.
      */
-    public static void main( String[] args ) {
-        /* Declaracion de variable tipo Partida */
+    public static void main(String[] args) {
         Partida game = new Partida();
         game.game();
     }
